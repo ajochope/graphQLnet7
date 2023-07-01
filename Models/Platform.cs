a037d527697dec53afc2ba657d5f6d94ef909f41
ajochope <ocalles@gmail.com>
2023-07-01 19:56:00 +0200

    Plaform model

---
+++ Models/Platform.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string LicenseKey { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Load configuration from appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
 
