3005dba27f0b503f46e6a5b49aa8d45c4222cbdc
ajochope <ocalles@gmail.com>
2023-07-02 19:39:43 +0200

    errorn in amespace name GraphQLVoyagerOptions

--- Program.cs
+++ Program.cs
@@ -3,29 +3,30 @@ using CommanderGQL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using GraphQL;
+using GraphQL.Server.Ui.Voyager;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Load configuration from appsettings.json
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-    // SQL Server DB Connection
-    // Console.WriteLine("--> Usando SQL Server DB");
-    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CommandConStr")));
+// SQL Server DB Connection
+// Console.WriteLine("--> Usando SQL Server DB");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CommandConStr")));
 
-    //GraphQL services for SQL Server DB
-    builder.Services.AddGraphQLServer()
-        .AddQueryType<Query>();
+//GraphQL services for SQL Server DB
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
 
-        // .AddMutationType<Mutation>()
-        // .AddSubscriptionType<Subscription>()
-        // .AddProjections()
-        // .AddFiltering()
-        // .AddSorting()
-        // .AddInMemorySubscriptions()
-        // .AddDataLoader<PlatformByIdDataLoader>()
-        // .AddDataLoader<CommandByIdDataLoader>()
-        // .AddDataLoader<PlatformByIdDataLoader>();
+    // .AddMutationType<Mutation>()
+    // .AddSubscriptionType<Subscription>()
+    // .AddProjections()
+    // .AddFiltering()
+    // .AddSorting()
+    // .AddInMemorySubscriptions()
+    // .AddDataLoader<PlatformByIdDataLoader>()
+    // .AddDataLoader<CommandByIdDataLoader>()
+    // .AddDataLoader<PlatformByIdDataLoader>();
 
 var app = builder.Build();
 
@@ -33,8 +34,14 @@ var app = builder.Build();
 app.UseRouting();
 
 // GraphQL endpoint Mapping
-app.MapGraphQL();
+app.MapControllers();
+app.MapGraphQL(path: "/graphql");
 
-app.MapGet("/", () => "Hello World!");
+// GraphQL Voyager .net 7
+app.UseGraphQLVoyager(new GraphQLVoyagerOptions()
+{
+    GraphQLEndPoint = "/graphql",
+    Path = "/graphql-voyager"
+});
 
 app.Run();
