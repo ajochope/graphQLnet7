b1a29f2eb9eb4a15f2babf1c59fbd4881e582981
ajochope <ocalles@gmail.com>
2023-07-02 11:52:02 +0200

    GraphQL Service

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Configuration;
 using CommanderGQL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using GraphQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,20 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnCh
     // SQL Server DB Connection
     // Console.WriteLine("--> Usando SQL Server DB");
     builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CommandConStr")));
+
+    //GraphQL services for SQL Server DB
+    builder.Services.AddGraphQLServer()
+        .AddQueryType<Query>();
+
+        // .AddMutationType<Mutation>()
+        // .AddSubscriptionType<Subscription>()
+        // .AddProjections()
+        // .AddFiltering()
+        // .AddSorting()
+        // .AddInMemorySubscriptions()
+        // .AddDataLoader<PlatformByIdDataLoader>()
+        // .AddDataLoader<CommandByIdDataLoader>()
+        // .AddDataLoader<PlatformByIdDataLoader>();
 
 var app = builder.Build();
 
